d465f31d7b684c0dff49ab345812e270af5abb72
Jaime Julio <jaime.julio@omni.pro>
2024-09-24 17:41:34 -0500

    tfstate

--- eks/eks-backend.tf
+++ eks/eks-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = var.s3_bucket_name
-    key            = "omni/terraform.tfstate" 
+    key            = "terraform.tfstate" 
     region         = var.region
     dynamodb_table = var.dynamodb_table_name
     encrypt        = true 
